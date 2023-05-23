1133039758787946adaae32fce3618f1a60a1dad
danielvillarg3 <danielvillarg3@github.com>
2023-05-23 15:44:58 -0500

    avance

--- mi_robot_vision/tomarFoto.py
+++ mi_robot_vision/tomarFoto.py
@@ -1,5 +1,5 @@
-import subprocess
-nombreNext = "prueba"
+#!/bin/bash
 
-time = subprocess.check_output('date')
-print ("Son las"+ str(time))
+DATE=$(date +"%Y-%m-%d_%H%M")
+
+fswebcam -r 1280.0x720.0 --no-banner /home/robotica/proyecto_ws/src/mi_robot_vision/mi_robot_vision/$DATE.jpg
