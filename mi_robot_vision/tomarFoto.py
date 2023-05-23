2dade42a3a2b0662f6721d1d2d58ba6a23661a8c
danielvillarg3 <danielvillarg3@github.com>
2023-05-23 16:49:02 -0500

    Nodo que publica video en ROS2

--- mi_robot_vision/tomarFoto.py
+++ mi_robot_vision/tomarFoto.py
@@ -1,5 +1,38 @@
-#!/bin/bash
+#!/usr/bin/python3
+import rclpy
+from rclpy.node import Node
+import cv2
+import os
+import sys
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
-DATE=$(date +"%Y-%m-%d_%H%M")
+class ImagePublisher(Node):
+    def __init__(self):
+        super().__init__("image_publisher")
+        print("Inicio del nodo que publica la imagen")
+        self.bridge = CvBridge()
+        self.cap = cv2.VideoCapture(0)
+        self.pub = self.create_publisher(Image, "/video", 10)
 
-fswebcam -r 1280.0x720.0 --no-banner /home/robotica/proyecto_ws/src/mi_robot_vision/mi_robot_vision/$DATE.jpg
+    def run(self):
+        while(self.cap.isOpened()):
+            ret, frame = self.cap.read() 
+            if ret:
+                self.pub.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
+            else:
+                self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+        self.cap.release()
+
+def main(args=None):
+    rclpy.init(args=args)
+    ip = ImagePublisher()
+    print("Publishing...")
+    ip.run()
+
+    ip.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
